b9eb6945b0614a26c449a16ad888e0b4a1b89f57
Jason <jconn99801@gmail.com>
2023-10-15 17:21:37 -0800

    created ProductList component

---
+++ client/src/ProductList.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products') // Replace with your actual API endpoint
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProductList;
