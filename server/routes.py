294a4aae961f67fd467fe3738b1879a6f45f7596
Jason <jconn99801@gmail.com>
2023-10-18 02:46:34 -0800

    combined redundant product routes

--- server/routes.py
+++ server/routes.py
@@ -43,101 +43,9 @@ def store_products():
     products = Product.query.filter_by(user_id=current_user.id).all()
     return render_template('store_products.html', products=products)
 
-# Allow store accounts to edit a product
-
-
-@main.route('/product/edit/<int:product_id>', methods=['GET', 'POST'])
-@login_required
-def edit_product(product_id):
-    if not current_user.is_seller:
-        flash('You do not have permission to edit products.', 'danger')
-        # Redirect to the home page or an error page
-        return redirect(url_for('main.index'))
-
-    product = Product.query.get(product_id)
-    if not product or product.user_id != current_user.id:
-        flash('Product not found or not owned by you.', 'danger')
-        # Redirect to the home page or an error page
-        return redirect(url_for('main.index'))
-
-    if request.method == 'POST':
-        product.name = request.form.get('name')
-        product.description = request.form.get('description')
-        product.price = request.form.get('price')
-        product.image = request.form.get('image')
-        product.inventory = request.form.get('inventory')
-        product.category = request.form.get('category')
-
-        db.session.commit()
-        flash('Product updated successfully!', 'success')
-        return redirect(url_for('main.store_products'))
-
-    return render_template('edit_product.html', product=product)
-
-
-# Allow store accounts to delete a product
-@main.route('/product/delete/<int:product_id>', methods=['POST'])
-@login_required
-def delete_product(product_id):
-    if not current_user.is_seller:
-        flash('You do not have permission to delete products.', 'danger')
-        # Redirect to the home page or an error page
-        return redirect(url_for('main.index'))
-
-    product = Product.query.get(product_id)
-    if not product or product.user_id != current_user.id:
-        flash('Product not found or not owned by you.', 'danger')
-        # Redirect to the home page or an error page
-        return redirect(url_for('main.index'))
-
-    db.session.delete(product)
-    db.session.commit()
-    flash('Product deleted successfully!', 'success')
-    return redirect(url_for('main.store_products'))
-
-
-@main.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        # Hash the password before storing it
-        hashed_password = bcrypt.generate_password_hash(
-            form.password.data).decode('utf-8')
-        user = User(email=form.email.data,
-                    password=hashed_password, name=form.name.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in.', 'success')
-        login_user(user)
-        return redirect(url_for('main.index'))
-    return render_template('register.html', form=form)
-
-
-@main.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
-            login_user(user)
-            return redirect(url_for('main.index'))
-        else:
-            flash('Login unsuccessful. Please check email and password.', 'danger')
-    return render_template('login.html', form=form)
-
-
-@main.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('main.index'))
-
-
 # Update an existing product
+
+
 @main.route('/product/edit/<int:product_id>', methods=['GET', 'POST'])
 @login_required
 def edit_product(product_id):
@@ -187,6 +95,47 @@ def delete_product(product_id):
     db.session.delete(product)
     db.session.commit()
     flash('Product deleted successfully!', 'success')
+    return redirect(url_for('main.store_products'))
+
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Hash the password before storing it
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(email=form.email.data,
+                    password=hashed_password, name=form.name.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
+        login_user(user)
+        return redirect(url_for('main.index'))
+    return render_template('register.html', form=form)
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
+            login_user(user)
+            return redirect(url_for('main.index'))
+        else:
+            flash('Login unsuccessful. Please check email and password.', 'danger')
+    return render_template('login.html', form=form)
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
     return redirect(url_for('main.index'))
 
 
