eed58f225fd85cbbfa72c4656546c84627fd8233
Jason <jconn99801@gmail.com>
2023-10-18 01:51:40 -0800

    updated routes to include register/login/logout

--- server/routes.py
+++ server/routes.py
@@ -1,8 +1,53 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
+from server.forms import RegistrationForm, LoginForm
+from server.models import User, Product, db
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return jsonify(message="Hello from Flask!")
+    # Replace with your welcome screen template
+    return render_template('index.html')
+
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Hash the password before storing it
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(email=form.email.data,
+                    password=hashed_password, name=form.name.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
+        login_user(user)
+        return redirect(url_for('main.index'))
+    return render_template('register.html', form=form)
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
+            login_user(user)
+            return redirect(url_for('main.index'))
+        else:
+            flash('Login unsuccessful. Please check email and password.', 'danger')
+    return render_template('login.html', form=form)
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
