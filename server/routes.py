9c4c2a2cd226a19c024b31090c799f9cce1f70e5
Jason <jconn99801@gmail.com>
2023-10-18 01:59:15 -0800

    added CRUD operations to routes

--- server/routes.py
+++ server/routes.py
@@ -51,3 +51,84 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+
+# Update an existing product
+@main.route('/product/edit/<int:product_id>', methods=['GET', 'POST'])
+@login_required
+def edit_product(product_id):
+    if not current_user.is_seller:
+        flash('You do not have permission to edit products.', 'danger')
+        # Redirect to the home page or an error page
+        return redirect(url_for('main.index'))
+
+    product = Product.query.get(product_id)
+    if not product:
+        flash('Product not found.', 'danger')
+        # Redirect to the home page or an error page
+        return redirect(url_for('main.index'))
+
+    if request.method == 'POST':
+        product.name = request.form.get('name')
+        product.description = request.form.get('description')
+        product.price = request.form.get('price')
+        product.image = request.form.get('image')
+        product.inventory = request.form.get('inventory')
+        product.category = request.form.get('category')
+
+        db.session.commit()
+        flash('Product updated successfully!', 'success')
+        return redirect(url_for('main.index'))
+
+    # Create an HTML template for the product editing form
+    return render_template('edit_product.html', product=product)
+
+# Delete an existing product
+
+
+@main.route('/product/delete/<int:product_id>', methods=['POST'])
+@login_required
+def delete_product(product_id):
+    if not current_user.is_seller:
+        flash('You do not have permission to delete products.', 'danger')
+        # Redirect to the home page or an error page
+        return redirect(url_for('main.index'))
+
+    product = Product.query.get(product_id)
+    if not product:
+        flash('Product not found.', 'danger')
+        # Redirect to the home page or an error page
+        return redirect(url_for('main.index'))
+
+    db.session.delete(product)
+    db.session.commit()
+    flash('Product deleted successfully!', 'success')
+    return redirect(url_for('main.index'))
+
+
+# Create a new product
+@main.route('/product/create', methods=['GET', 'POST'])
+@login_required
+def create_product():
+    if not current_user.is_seller:
+        flash('You do not have permission to create products.', 'danger')
+        # Redirect to the home page or an error page
+        return redirect(url_for('main.index'))
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        description = request.form.get('description')
+        price = request.form.get('price')
+        image = request.form.get('image')
+        inventory = request.form.get('inventory')
+        category = request.form.get('category')
+
+        product = Product(name=name, description=description, price=price,
+                          image=image, inventory=inventory, category=category)
+        db.session.add(product)
+        db.session.commit()
+        flash('Product created successfully!', 'success')
+        return redirect(url_for('main.index'))
+
+    # Create an HTML template for the product creation form
+    return render_template('create_product.html')
