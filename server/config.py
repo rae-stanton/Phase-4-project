57a011976b091760a1a28e6e80020c062defcde7
Jason <jconn99801@gmail.com>
2023-10-18 01:14:33 -0800

    updated config.py

--- server/config.py
+++ server/config.py
@@ -1,2 +1,9 @@
 # Database configuration
+
+# Flask app configuration
+DEBUG = False  # Set to False in production
+# SECRET_KEY = 'your_secret_key_here'
+
+# SQLAlchemy configuration
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/shoppingapp.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
