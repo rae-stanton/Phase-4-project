ad0198b43602a93acf6cf2ff8f823f106c46c6db
Jason <jconn99801@gmail.com>
2023-10-18 00:57:47 -0800

    imported Bcrypt to server __init__

--- server/__init__.py
+++ server/__init__.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 # Declare the extensions outside the function, but don't initialize them yet
 db = SQLAlchemy()
 migrate = Migrate()
+bcrypt = Bcrypt()
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shoppingapp.db'
+    # app.config['SECRET_KEY'] = 'your_secret_key_here'
 
     # Initialize the extensions with the app inside the function
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
     # Import models to ensure they're attached to the SQLAlchemy instance
     from . import models
