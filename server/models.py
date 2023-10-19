0fe8e2502041b7b390c56d5fdffe20c8997af8d7
Jason <jconn99801@gmail.com>
2023-10-18 20:49:24 -0800

    fixed model imports

--- server/models.py
+++ server/models.py
@@ -19,7 +19,7 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(41), unique=True, nullable=False)
+    name = db.Column(db.String(41), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     is_seller = db.Column(db.Boolean, default=False)
