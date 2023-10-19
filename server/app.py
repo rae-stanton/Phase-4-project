28e4ccd97c9764e0920c49efff0479d7de849726
Jason <jconn99801@gmail.com>
2023-10-18 22:58:58 -0800

    importing Product from models to set-up prod-CLS

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
-from models import User, db  # assuming models.py is in the same directory
+from models import User, Product, db
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
