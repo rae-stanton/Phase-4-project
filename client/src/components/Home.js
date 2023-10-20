ea92e3fa6cb8a5eff6b4d34efeb89a4ecfd12c7d
Jason <jconn99801@gmail.com>
2023-10-20 10:02:41 -0800

    Updated home to display products

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import "./Home.css"
+import React, { useState } from "react";
+import HomeHeader from "./HomeHeader.js";
+import ProductList from "./ProductList.js";
+import "./Home.css";
 
 function Home() {
-  return <h1>Welcome to our 100x better Etsy!</h1>;
+  const [searchInput, setSearchInput] = useState("");
+  return (
+    <div>
+      <HomeHeader searchInput={searchInput} setSearchInput={setSearchInput} />
+      <ProductList searchInput={searchInput} />
+    </div>
+  );
 }
 
 export default Home;
