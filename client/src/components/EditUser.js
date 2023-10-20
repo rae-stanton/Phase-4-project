c3d2cea689b89d669d3c204b2e353916caea165d
Rae Stanton <raestanton93@gmail.com>
2023-10-20 09:57:53 -0700

    allows deletion of user
    with redirect using useNavigate hook

--- client/src/components/EditUser.js
+++ client/src/components/EditUser.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Formik, Field, Form } from "formik";
 import Button from "react-bootstrap/Button";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function EditUser(props) {
   const { userId } = useParams();
+  const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
 
@@ -26,6 +27,31 @@ function EditUser(props) {
 
     fetchUserData();
   }, [userId]);
+
+  const deleteUser = async () => {
+    if (!window.confirm("Are you sure you want to delete this user?")) return;
+
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/users/${userId}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("Deletion failed.");
+      }
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        alert("User deleted successfully!");
+        navigate("/"); // Modify this as per your need
+      } else {
+        alert(data.message || "Deletion failed.");
+      }
+    } catch (err) {
+      alert(err.message);
+    }
+  };
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -73,8 +99,6 @@ function EditUser(props) {
 
               if (response.status === 200) {
                 alert("User updated successfully!");
-                // Optionally re-fetch user data
-                // fetchUserData();
               } else {
                 alert(data.message || "Update failed.");
               }
@@ -83,40 +107,51 @@ function EditUser(props) {
             }
           }}
         >
-          <Form className="form-border">
-            <label htmlFor="firstName">First Name</label>
-            <br />
-            <Field id="firstName" name="firstName" placeholder="Luna" />
-            <br />
-            <label htmlFor="lastName">Last Name</label>
-            <br />
-            <Field id="lastName" name="lastName" placeholder="Lucy" />
-            <br />
-            <label htmlFor="email">Email</label>
-            <br />
-            <Field
-              id="email"
-              name="email"
-              placeholder="Luna@acme.com"
-              type="email"
-            />
-            <br />
-            <label htmlFor="password">
-              New Password (leave blank if unchanged)
-            </label>
-            <br />
-            <Field
-              id="password"
-              name="password"
-              placeholder="********"
-              type="password"
-            />
-            <br />
-            <Button variant="primary" type="submit" className="form-button">
-              Update
-            </Button>
-            <br />
-          </Form>
+          {() => (
+            // Added an arrow function here to be able to include multiple JSX elements
+            <>
+              <Form className="form-border">
+                <label htmlFor="firstName">First Name</label>
+                <br />
+                <Field id="firstName" name="firstName" placeholder="Luna" />
+                <br />
+                <label htmlFor="lastName">Last Name</label>
+                <br />
+                <Field id="lastName" name="lastName" placeholder="Lucy" />
+                <br />
+                <label htmlFor="email">Email</label>
+                <br />
+                <Field
+                  id="email"
+                  name="email"
+                  placeholder="Luna@acme.com"
+                  type="email"
+                />
+                <br />
+                <label htmlFor="password">
+                  New Password (leave blank if unchanged)
+                </label>
+                <br />
+                <Field
+                  id="password"
+                  name="password"
+                  placeholder="********"
+                  type="password"
+                />
+                <br />
+                <Button variant="primary" type="submit" className="form-button">
+                  Update
+                </Button>
+                <Button
+                  variant="danger"
+                  onClick={deleteUser}
+                  className="delete-button"
+                >
+                  Delete User
+                </Button>
+              </Form>
+            </>
+          )}
         </Formik>
       </div>
     </div>
