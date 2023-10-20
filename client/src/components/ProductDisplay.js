32d0345c7e1b62e83970f3aca9a2413dc851c28a
Jason <jconn99801@gmail.com>
2023-10-20 10:05:26 -0800

    Removed anything in ProductDisplay (for now)

--- client/src/components/ProductDisplay.js
+++ client/src/components/ProductDisplay.js
@@ -1,45 +1,7 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import "./RecipeDisplay.css";
-import ordersImage from "../images/orders.png"; // <-- Import the image here
-import ProductList from "./ProductList.js";
 
-function ProductDisplay({ product, onClose }) {
-  return (
-    <div>
-      <h1>Welcome! Here's your info:</h1>
-      <DashCardGrid1 />
-    </div>
-  );
+function ProductDisplay() {
+  return <div></div>;
 }
 
-function DashCardGrid1() {
-  return (
-    <div className="card-container">
-      <Row xs={1} md={3} className="g-3">
-        {Array.from({ length: 3 }).map((_, idx) => (
-          <Col key={idx}>
-            <Card className="dash-cards">
-              <Card.Img
-                variant="top"
-                src={ordersImage}
-                className="dash-card-image"
-              />
-
-              <Card.Body>
-                <Card.Title>Check this out:</Card.Title>
-                <Card.Text>
-                  This is whatever thing you will want to find.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
-}
-
-export default Dashboard;
+export default ProductDisplay;
